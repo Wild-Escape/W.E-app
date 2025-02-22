461b047ab1be787ffbb5ade009c92159c1319547
Vladys K <vldk0022@gmail.com>
2025-02-22 01:02:51 +0100

    feat: working on displaying the favorite experiences of a user

--- src/services/favorite.service.js
+++ src/services/favorite.service.js
@@ -3,3 +3,4 @@ import { createHttp } from "./base.service";
 const http = createHttp(true);
 
 export const addToFavoriteService = (id) => http.post(`/experiences/${id}/favorite`);
+export const getFavoritesService = () => http.get("/favorites");
