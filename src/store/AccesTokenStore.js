49f2db5450d7357d3f1c9e4c0eab4aa4a704761f
Vladys K <vldk0022@gmail.com>
2025-03-17 20:20:04 +0100

    modified logout route

--- src/store/AccesTokenStore.js
+++ src/store/AccesTokenStore.js
@@ -16,5 +16,5 @@ export const getAccessToken = () => {
 export const logout = () => {
   localStorage.removeItem(JWT_TOKEN_KEY);
 
-  window.location.assign("/login");
+  window.location.assign("/common/login");
 };
